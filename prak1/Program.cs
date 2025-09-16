28d2130c6d2c4869b89196abc4f19eed2374bc8e
N4K <pronin6030@gmail.com>
2025-09-16 15:45:18 +0300

    ChangeREADME

--- prak1/Program.cs
+++ prak1/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        string startMessage = "Добро пожаловать в калькулятор! Введите операцию (+, -, *, /,%, 1/x, x^2, sqrt, MS, M+, M-, MR) и два значения.\n" +
+        string startMessage = "Добро пожаловать в калькулятор! Введите операцию (+, -, *, /,%, 1/x, x^2, sqrt, MS, M+, M-, MR) и одно/два значения.\n" +
             "После первой операции вводите операцию и одно число, либо команду clear для сброса.\n" +
             "Введите команду exit для выхода";
         string oper = "start";
@@ -16,17 +16,19 @@
 
         while(oper != "exit")
         {
-            //При вводе exit в начале цикла работы программа не завершится, а будет ожидать ввода num1
+            Console.Write("Введите операцию: ");
             oper = Console.ReadLine();
             if (oper == "clear")
             {
                 isClear = true;
                 Console.Clear();
                 Console.WriteLine(startMessage);
+                Console.Write("Введите операцию: ");
                 oper = Console.ReadLine();
             }
             if (isClear)
             {
+                Console.Write("Введите число: ");
                 num1 = float.Parse(Console.ReadLine());
                 result.Item1 = num1;
                 isClear = false;
@@ -34,27 +36,44 @@
             switch (oper)
             {
                 case ("+"):
+                    Console.Write("Введите число: ");
                     num2 = float.Parse(Console.ReadLine());
                     result.Item1 = result.Item1 + num2;
                     break;
                 case ("-"):
+                    Console.Write("Введите число: ");
                     num2 = float.Parse(Console.ReadLine());
                     result.Item1 = result.Item1 - num2;
                     break;
                 case ("*"):
+                    Console.Write("Введите число: ");
                     num2 = float.Parse(Console.ReadLine());
                     result.Item1 = result.Item1 * num2;
                     break;
                 case ("/"):
+                    Console.Write("Введите число: ");
                     num2 = float.Parse(Console.ReadLine());
-                    result.Item1 = result.Item1 / num2;
+                    if (num2 == 0)
+                    {
+                        result.Item2 = "Деление на ноль!";
+                    }else{
+                        result.Item1 = result.Item1 / num2;
+                    }
                     break;
                 case ("%"):
+                    Console.Write("Введите число: ");
                     num2 = float.Parse(Console.ReadLine());
                     result.Item1 = result.Item1 % num2;
                     break;
                 case ("1/x"):
-                    result.Item1 = 1 / result.Item1;
+                    if (result.Item1 == 0)
+                    {
+                        result.Item2 = "Деление на ноль!";
+                    }
+                    else
+                    {
+                        result.Item1 = 1 / result.Item1;
+                    }
                     break;
                 case ("sqrt"):
                     result.Item1 = (float)Math.Sqrt(result.Item1);
@@ -77,12 +96,12 @@
                     result.Item1 = result.Item1 - m;
                     break;
                 case ("exit"):
-                    break;
+                    continue;
                 default:
                     result.Item2 = "Введены некорректные данные. Повторите ввод данных.\n";
                     break;
             }
-
+            Console.WriteLine("123");
             if (result.Item2 == "OK") {
                 Console.WriteLine($"Ответ: {result.Item1}");
             } else {
